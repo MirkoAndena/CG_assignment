c117ca26606f17487e368607ae8af23c0233ae82
Mirko <1751135-mirkoandena@users.noreply.gitlab.com>
2021-07-20 14:43:46 +0200

    commenti ad A16

--- A16 - Scene/scene.js
+++ A16 - Scene/scene.js
@@ -1,5 +1,6 @@
 var matrices = [ ];
 
+// S: componenti con (x, y, z, rx, ry, rz, first_child, last_child)
 function drawSceneTree(S) {
 	var i;
 	for(i = 0; i < S.length; i++) {
@@ -7,6 +8,7 @@ function drawSceneTree(S) {
 		let childMatrix = getMatrixOf(S, i);
 		let matrix = utils.multiplyMatrices(parentMatrix, childMatrix);
 
+		// salvataggio delle matrici di ogni elemento 
 		matrices.push(matrix);
 		draw(i, matrix);
 	}
@@ -16,6 +18,7 @@ function drawSceneTree(S) {
 }
 
 function getMatrixOf(S, index) {
+	// creazione della matrice traslazione/rotazione di un elemento
 	return mul(
 		utils.MakeTranslateMatrix(S[index][0], S[index][1], S[index][2]),
 		utils.MakeRotateZMatrix(S[index][5]),
@@ -25,6 +28,7 @@ function getMatrixOf(S, index) {
 
 function getParent(S, index) {
 	let candidate;
+	// per ogni elemento controllo se è presente nei figli di qualcuno
 	for (candidate = 0; candidate < S.length; candidate++)
 		if (S[candidate][6] <= index && index <= S[candidate][7])
 			return candidate
